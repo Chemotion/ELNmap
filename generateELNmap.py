78c1f058967166ad9a959049b0d82df9a414234a
Shashank Shekhar Harivyasi <harivyasi@users.noreply.github.com>
2022-02-10 14:25:35 +0100

    v2
    - GH action to create map
    - locations in JSON
    - extensible
    - commented

--- generateELNmap.py
+++ generateELNmap.py
@@ -1,87 +1,88 @@
 import matplotlib.pyplot as plt
-import matplotlib
-import mplcursors
+import matplotlib.font_manager as mplf
+import mplcursors as mplc
+import pandas as pd
 import geopandas as gpd
+from pathlib import Path
 
-data_dir = "data/"
-path = data_dir + "NUTS_RG_01M_2021_3035_LEVL_3.json"
-pathBorders = data_dir + "NUTS_RG_01M_2021_3035_LEVL_1.json"
+# data directory
+data_dir  = Path("data")
+font_path = Path("OpenSans-Bold.ttf")
 
-map_CH = gpd.read_file(path)
-map_CH = map_CH[map_CH.CNTR_CODE == "CH"]
+# load font
+font = mplf.FontProperties(fname=font_path)
 
-map_borders = gpd.read_file(pathBorders)
-map_borders = map_borders[map_borders.CNTR_CODE == "DE"]
+# NUTS level 1 is 'states' or 'group of states' or something similar
+# NUTS level 3 is (usually) city-level boundary
 
-map_df = gpd.read_file(path)
-map_df = map_df[map_df.CNTR_CODE == "DE"]
-map_df = map_df.append(map_CH)
+jsons = {"country":  data_dir / "NUTS_RG_01M_2021_3035_LEVL_1.json",
+         "city"   :  data_dir / "NUTS_RG_01M_2021_3035_LEVL_3.json",
+         "plotted":  data_dir / "plotted_locations.json"}
 
-fig, ax = plt.subplots(1, figsize=(20, 12))
-map_df['coords'] = map_df['geometry'].apply(lambda x: x.representative_point().coords[:])
-map_df['coords'] = [coords[0] for coords in map_df['coords']]
-map_df.plot(ax=ax, color='lightblue')
+connections = [("Karlsruhe","Aachen")] # can be moved to a separate file if required
 
-cities = []
-cities.append(["Aachen","Städteregion Aachen","productive",4])
-cities.append(["Braunschweig", "Braunschweig, Kreisfreie Stadt", "testing",2])
-cities.append(["Chemnitz", "Chemnitz, Kreisfreie Stadt","planned",1])
-cities.append(["Dresden","Dresden, Kreisfreie Stadt","testing",1])
-cities.append(["Duisburg-Essen","Duisburg, Kreisfreie Stadt","productive",1])
-cities.append(["Freiburg","Freiburg im Breisgau, Stadtkreis","testing",1])
-cities.append(["Greifswald","Vorpommern-Greifswald","productive",1])
-cities.append(["Heidelberg","Heidelberg, Stadtkreis","productive",1])
-cities.append(["Jena","Jena, Kreisfreie Stadt","productive",1])
-cities.append(["Kaiserslautern","Kaiserslautern, Kreisfreie Stadt","productive",1])
-cities.append(["Karlsruhe","Karlsruhe, Stadtkreis","mixed",5])
-cities.append(["Kiel","Kiel, Kreisfreie Stadt","planned",1])
-cities.append(["Köln","Köln, Kreisfreie Stadt","testing",1])
-cities.append(["Lausanne","Genève","productive",1])
-cities.append(["Mainz","Mainz, Kreisfreie Stadt","testing",1])
-cities.append(["Paderborn","Paderborn","productive",1])
-cities.append(["Stuttgart","Stuttgart, Stadtkreis","productive",2])
-cities.append(["Zürich","Zürich","mixed",3])
+# draw the canvas
+fig, ax = plt.subplots(1, figsize=(15,9), tight_layout=True)
 
-# mixed: Karlsruhe, Zürich
+# read all locations
+locations = pd.read_json(jsons["plotted"])
 
-xyKarlsruhe = map_df[map_df.NAME_LATN == "Karlsruhe, Stadtkreis"]['coords'].values[0]
-xyAachen = map_df[map_df.NAME_LATN == "Städteregion Aachen"]['coords'].values[0]
+# determine included countries
+countries_included = locations["country_code"].unique().tolist()
 
-con_repos, = plt.plot([xyKarlsruhe[0], xyAachen[0]], [xyKarlsruhe[1], xyAachen[1]], color='red', linestyle=":", linewidth=2, zorder=2)
+# read country boundaries
+map_country = gpd.read_file(jsons["country"])
+map_country = map_country[map_country.CNTR_CODE.isin(countries_included)]
 
-font_path = "Open_Sans/static/OpenSans/OpenSans-Bold.ttf"
-prop = matplotlib.font_manager.FontProperties(fname=font_path)
+# modifications to map, if any
+if "FR" in countries_included:
+    # if possible, include only mainland for conciseness
+    map_country.drop(map_country[map_country.FID.isin(["FRY","FRM"])].index, inplace=True)
 
-for idx, row in map_df.iterrows():
-    for city in cities:
-        if row['NAME_LATN'] == city[1]:
-            if city[2] == "productive" or city[2] == "mixed":
-                # city name with number of user = city[3]) + ' ' + city[0]
-                plt.annotate(text=str(city[3]), xy=row['coords'], horizontalalignment='center', verticalalignment='center', color='white', fontproperties=prop,  zorder=idx+3)
-                c_green = plt.Circle(row['coords'], 20000, color='g', zorder=idx+2)
-                ax.add_patch(c_green)                
-            elif city[2] == "testing":
-                plt.annotate(text=city[3], xy=row['coords'], horizontalalignment='center', verticalalignment='center', color='black', fontproperties=prop,  alpha=1)
-                c_orange = plt.Circle(row['coords'], 20000, color='orange')
-                ax.add_patch(c_orange)                
-            elif city[2] == "planned":
-                plt.annotate(text=city[3], xy=row['coords'], horizontalalignment='center', verticalalignment='center', color='white', fontproperties=prop)                
-                c_blue = plt.Circle(row['coords'], 20000, color='b')
-                ax.add_patch(c_blue)                
-            # elif city[2] == "mixed":
-            #     plt.annotate(text=city[3], xy=row['coords'], horizontalalignment='center', verticalalignment='center', color='w', fontproperties=prop)
-            #     circle = matplotlib.patches.Wedge(row['coords'], 20000, 45, 225, fc='g')
-            #     ax.add_patch(circle)
-            #     circle = matplotlib.patches.Wedge(row['coords'], 20000, 225, 45, fc='orange')
-            else:       
-                plt.annotate(text=city[3], xy=row['coords'], horizontalalignment='center', verticalalignment='center', color='b', fontproperties=prop)            
-                c_white = plt.Circle(row['coords'], 20000, color='w')
-                ax.add_patch(c_white)
+# plot countries map
+map_country.plot(ax=ax, column="CNTR_CODE", cmap='tab20', edgecolor='w')
 
-map_borders.boundary.plot(color='w', ax=ax, zorder=1)
+# read city boundaries, subset to countries included
+map_city = gpd.read_file(jsons["city"])
+map_city = map_city[map_city.CNTR_CODE.isin(countries_included)]
 
-plt.legend([con_repos, c_green, c_orange, c_blue], ['Connected Repositories', 'Productive', 'Testing', 'Planned'], loc='upper right').set_draggable(True)
-cursor = mplcursors.cursor(multiple=True)
+# find the representative point
+map_city['coords'] = map_city['geometry'].apply(lambda x: x.representative_point().coords[0])
 
+# set color for patches
+color = {"production" : "green",
+         "mixed"      : "green", 
+         "testing"    : "orange",
+         "planned"    : "blue"}
+
+# plot all connections
+tab10 = plt.cm.get_cmap("tab10", 10) # color map for connections
+for idx, connection in enumerate(connections):
+    polygon = []
+    for city in connection:
+        lvl3_name = locations[locations['common_name'] == city]['nuts_lvl3'].values[0]
+        polygon.append(map_city[map_city['NAME_LATN'] == lvl3_name]['coords'].values[0])
+    polygon.append(polygon[0]) #repeat the first point to create a 'closed loop' # see https://stackoverflow.com/questions/43971259/
+    xs, ys = zip(*polygon)
+    conn, = plt.plot(xs, ys, color=tab10(idx), ls=":", lw=2)
+
+# plot all the cities
+used_patches = []
+for idx, row in locations.iterrows():
+    coord = map_city[map_city['NAME_LATN'] == row['nuts_lvl3']]['coords'].values[0]
+    ax.add_patch(plt.Circle(coord, 20000, color=color[row["stage"]]))
+    used_patches.append(row["stage"])
+    plt.annotate(text = row["num_users"], xy = coord, horizontalalignment='center', verticalalignment='center', color='white', fontproperties=font)
+
+# create patches for legend
+used_patches = set(used_patches)
+used_patches.remove("mixed") # ignore mixed as the color is same as production
+patches_for_legend = [[], []]
+for patch in used_patches:
+    patches_for_legend[0].append(patch.title())
+    patches_for_legend[1].append(plt.Circle(coord, 20000, color=color[patch]))
+
+plt.legend([conn]+patches_for_legend[1], ['Connected Repositories']+patches_for_legend[0]).set_draggable(True)
+cursor = mplc.cursor(multiple=True)
 plt.axis('off')
-plt.show()
+plt.savefig("map.svg")
