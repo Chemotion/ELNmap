3317e97034d5f67fedecdbdbf2f877569f718715
Shashank S. Harivyasi <45761248+harivyasi@users.noreply.github.com>
2023-08-15 13:56:39 +0000

    rewrite - support for international locations - removed all references to connections - cleaner code for static plotting - uses panda df to full extent - produces two static maps - DE and EUR

--- download_prerequistes.sh
+++ download_prerequistes.sh
@@ -5,5 +5,6 @@ cd data
 # download and save the font
 wget -N https://github.com/googlefonts/opensans/raw/main/fonts/ttf/OpenSans-Bold.ttf
 # download and save the topojson files
-wget -N https://gisco-services.ec.europa.eu/distribution/v2/nuts/topojson/NUTS_RG_01M_2021_3035_LEVL_1.json
-wget -N https://gisco-services.ec.europa.eu/distribution/v2/nuts/topojson/NUTS_RG_01M_2021_3035_LEVL_3.json
+wget -N https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_01M_2021_4326_LEVL_1.geojson
+wget -N https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_01M_2021_4326_LEVL_3.geojson
+wget -N https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_10m_populated_places_simple.geojson
